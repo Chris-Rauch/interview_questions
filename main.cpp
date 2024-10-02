83411b3e352ac68b1af6add31ca12e03b9152c8c
Chris-Rauch <rauch.christopher13@gmail.com>
2024-10-02 15:52:58 -0700

    Added files to .gitignore

--- main.cpp
+++ main.cpp
@@ -8,13 +8,40 @@ using namespace std;
 #include "palindromePermutation.h"
 #include "levenshtein.h"
 #include "strCompression.h"
+#include "rotateMatrix.h"
 
 int main()
 {
-    string str = "aabcccccaaa";
-    string otherStr = compressStringV2(str);
+    /* int** 3x3
+    int len = 3;
+    int dist = 4;
+    int* matrix[3] = {nullptr};
+    for(int i = 0; i < len; ++i)
+    {
+        matrix[i] = new int[len];
+    }  
+    for(int i = 0; i < len; ++i)
+    {
+        for(int j = 0; j < len; ++j)
+        {
+            matrix[i][j] = ((i*len) + j);
+        }
+    }
+    */
+    vector<vector<int>> matrix(3,vector<int>(4));
+    for(int i = 0; i < matrix.size(); ++i)
+    {
+        for(int j = 0; j < matrix.front().size(); ++j)
+        {
+            matrix[i][j] = ((i*4) + j);
+        }
+    }
+    cout << "Initial matrix\n";
+    printMatrix(matrix);
+    rotateMatrixInPlace(matrix);
+    cout << "After Rotate\n";
+    printMatrix(matrix);
 
-    cout << "Other String: " << otherStr << endl;
 
     return 0;
 }
